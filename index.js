b6a70f910450c63ad0fd5427be7cedf02351f6a2
aniket <escaperealityproject@gmail.com>
2018-06-15 17:01:25 +0530

    Enter JS B)

---
+++ index.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Enter First Name");
+var secondName = prompt("Enter Second Name");
+var age = prompt("Enter Age");
+console.log("Your Name is " + firstName + " " + secondName)
+console.log("You are " + age + " years old");
