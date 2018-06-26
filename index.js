b2fdff412ab62856e83aaec83cbc8f457b275d24
aniket <escaperealityproject@gmail.com>
2018-06-26 14:50:50 +0530

    Color Game, centered boxes

--- index.js
+++
@@ -1,5 +0,0 @@
-var firstName = prompt("Enter First Name");
-var secondName = prompt("Enter Second Name");
-var age = prompt("Enter Age");
-console.log("Your Name is " + firstName + " " + secondName)
-console.log("You are " + age + " years old");
