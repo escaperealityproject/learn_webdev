705c2c0e8d16e4ec95ec8a8112883abee0df95e5
aniket <escaperealityproject@gmail.com>
2018-06-27 16:25:23 +0530

    Added new colors/game button

--- index.js
+++ index.js
@@ -2,19 +2,27 @@ var squares = document.querySelectorAll(".square");
 var colors = generateRandomColors(6);
 var pickedColor = pickColor();
 var messageDisplay = document.querySelector("#message");
+var resetButton = document.querySelector("#reset");
 
 document.getElementById("colorDisplay").textContent = pickedColor;
 
-
+resetButton.addEventListener("click",function(){
+  resetButton.textContent= "New Colors";
+  colors = generateRandomColors(6);
+  giveRandColors();
+  pickedColor = pickColor();
+  document.getElementById("colorDisplay").textContent = pickedColor;
+})
 
 for (var i = 0; i < colors.length; i++) {
   squares[i].style.backgroundColor = colors[i];
+
   squares[i].addEventListener("click", function() {
     var clickedColor = this.style.backgroundColor;
-
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct";
       changeColors(pickedColor);
+      resetButton.textContent= "Play Again!";
     } else {
       this.style.backgroundColor = "#232323"
       messageDisplay.textContent = "Wrong";
@@ -32,15 +40,20 @@ function pickColor() {
   return colors[giveRandNumUpto(colors.length)];
 }
 
-function generateRandomColors(num){
-  giveRandNumUpto(256);
-  var colors=[];
-  for(var i=0;i<num;i++){
-    colors[i]="rgb("+giveRandNumUpto(256)+", "+giveRandNumUpto(256)+", "+giveRandNumUpto(256)+")";
+function generateRandomColors(num) {
+  var colors = [];
+  for (var i = 0; i < num; i++) {
+    colors[i] = "rgb(" + giveRandNumUpto(256) + ", " + giveRandNumUpto(256) + ", " + giveRandNumUpto(256) + ")";
   }
   return colors;
 }
 
-function giveRandNumUpto(num){
+function giveRandNumUpto(num) {
   return Math.floor(Math.random() * num);
+}
+
+function giveRandColors() {
+  for (var i = 0; i < colors.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
 }
