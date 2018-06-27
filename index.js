0cda29b509d9f04f228cbf7136cd37bf784d38da
aniket <escaperealityproject@gmail.com>
2018-06-27 17:05:51 +0530

    Added easy/hard mode

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 var squares = document.querySelectorAll(".square");
-var colors = generateRandomColors(6);
-var pickedColor = pickColor();
+var colors;
+var pickedColor;
 var messageDisplay = document.querySelector("#message");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+var numSquares = 6;
+
+genPickColors(numSquares);
+
+easyBtn.addEventListener("click", function() {
+  this.classList.add("selected");
+  hardBtn.classList.remove("selected");
+  numSquares=3;
+  genPickColors(numSquares);
+  for (var i = numSquares; i < squares.length; i++) {
+    squares[i].style.display = "none";
+  }
+})
+
+hardBtn.addEventListener("click", function() {
+  this.classList.add("selected");
+  easyBtn.classList.remove("selected");
+  numSquares=6;
+  genPickColors(numSquares);
+  for (var i = 3; i < squares.length; i++) {
+    squares[i].style.display = "block";
+  }
+})
 
 document.getElementById("colorDisplay").textContent = pickedColor;
 
-resetButton.addEventListener("click",function(){
-  resetButton.textContent= "New Colors";
-  colors = generateRandomColors(6);
-  giveRandColors();
-  pickedColor = pickColor();
-  document.getElementById("colorDisplay").textContent = pickedColor;
+resetButton.addEventListener("click", function() {
+  resetButton.textContent = "New Colors";
+  genPickColors(numSquares);
 })
 
 for (var i = 0; i < colors.length; i++) {
@@ -22,7 +44,8 @@ for (var i = 0; i < colors.length; i++) {
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct";
       changeColors(pickedColor);
-      resetButton.textContent= "Play Again!";
+      resetButton.textContent = "Play Again!";
+      document.getElementById("colorDisplay").style.backgroundColor = pickedColor;
     } else {
       this.style.backgroundColor = "#232323"
       messageDisplay.textContent = "Wrong";
@@ -45,6 +68,9 @@ function generateRandomColors(num) {
   for (var i = 0; i < num; i++) {
     colors[i] = "rgb(" + giveRandNumUpto(256) + ", " + giveRandNumUpto(256) + ", " + giveRandNumUpto(256) + ")";
   }
+  for (var i = 0; i < colors.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
   return colors;
 }
 
@@ -52,8 +78,10 @@ function giveRandNumUpto(num) {
   return Math.floor(Math.random() * num);
 }
 
-function giveRandColors() {
-  for (var i = 0; i < colors.length; i++) {
-    squares[i].style.backgroundColor = colors[i];
-  }
+
+function genPickColors(num) {
+  colors = generateRandomColors(num);
+  pickedColor = pickColor();
+  document.getElementById("colorDisplay").textContent = pickedColor;
+  document.getElementById("colorDisplay").style.backgroundColor = "#232323";
 }
